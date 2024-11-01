9d42b974bdd9b1ef110f35bf5268ff4327c12088
cross-sniper <cts.contact.br@gmail.com>
2024-10-31 22:30:29 -0300

    renamed Sidebar to counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,24 @@ import React from "./react-zero";
 import "./stylesheet.css";
 
 // Sidebar component definition
-function Sidebar(attrs: { count: number; setCount: Function }) {
+function Counter() 
+{
+  const [count, setCount] = React.useState(0); // Initial count state
+
   return (
     <div>
-      <div>Count: {attrs.count}</div>
-      <button onClick={() => attrs.setCount(attrs.count + 1)}>Click me</button>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
 
 // Main App component
 function App() {
-  const [count, setCount] = React.useState(0); // Initial count state
 
   return (
     <>
-      <Sidebar count={count} setCount={setCount} />
+      <Counter/>
       <button onClick={() => alert("Hello!")}>
         Hello, world
       </button>
